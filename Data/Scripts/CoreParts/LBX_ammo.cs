ab7abb883c04cc956561e769c882bc1fd0a6b906
BadM0dder <npool@emich.edu>
2023-04-04 20:29:42 -0400

    shield damage up

--- Data/Scripts/CoreParts/LBX_ammo.cs
+++ Data/Scripts/CoreParts/LBX_ammo.cs
@@ -903,7 +903,7 @@ namespace Scripts
                 },
                 Shields = new ShieldDef
                 {
-                    Modifier = 1.4f, // Multiplier for damage against shields.
+                    Modifier = 4f, // Multiplier for damage against shields.
                     Type = Default, // Damage vs healing against shields; Default, Heal
                     BypassModifier = -1f, // If greater than zero, the percentage of damage that will penetrate the shield.
                 },
@@ -2836,7 +2836,7 @@ namespace Scripts
                 },
                 Shields = new ShieldDef
                 {
-                    Modifier = 2f, // Multiplier for damage against shields.
+                    Modifier = 10f, // Multiplier for damage against shields.
                     Type = Default, // Damage vs healing against shields; Default, Heal
                     BypassModifier = -1f, // If greater than zero, the percentage of damage that will penetrate the shield.
                 },
